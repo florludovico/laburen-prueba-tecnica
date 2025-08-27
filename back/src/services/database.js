305057dfdb4789a1500ddc7a999dbea31c326028
florludovico <florencia.ludovico@gmail.com>
2025-08-27 19:15:05 -0300

    Correccion de error para deploy en render (II)

--- back/src/services/database.js
+++ back/src/services/database.js
@@ -1,14 +1,17 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const connectionString = process.env.POSTGRES_URL;
+const connectionString = process.env.POSTGRES_URL || process.env.DATABASE_URL;
+
 
 const pool = new Pool({
-  connectionString: connectionString,
+  connectionString,
   ssl: {
+    require: true,
     rejectUnauthorized: false
   },
 });
+
 
 module.exports = {
   pool,
